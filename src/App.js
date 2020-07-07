a8ee218bd789ba3b429aa7988adbb47f60dd39fb
LoicBrassart <loic.brassart@gmail.com>
2020-07-07 14:31:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { backend } from "./conf";
 
 function App() {
+  const [trips, setTrips] = useState([]);
+  const [newTrip, setNewTrip] = useState({});
+
+  useEffect(() => {
+    console.log("Backend: ", backend);
+    axios.get(`${backend}/trips`).then(({ data }) => {
+      setTrips(
+        data
+          .sort((tripA, tripB) => {
+            if (!tripA.price) return 1;
+            if (!tripB.price) return -1;
+
+            return tripA.price - tripB.price;
+          })
+          .slice(0, 10)
+      );
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    const tmp = {
+      ...newTrip,
+      [e.target.name]: e.target.value,
+    };
+    setNewTrip(tmp);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${backend}/trips`, newTrip)
+      .then()
+      .catch((err) => {
+        console.log("Erreur:", err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {trips.map((trip, i) => {
+          return (
+            <li key={i}>
+              {trip.label}
+              {trip.price && ` (${trip.price}€)`}
+            </li>
+          );
+        })}
+      </ul>
+
+      <h2>New trip</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          name="label"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="number"
+          name="price"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input type="submit" value="New Trip!" />
+      </form>
     </div>
   );
 }
